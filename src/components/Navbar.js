80f3393fc2775bd1b8a8003b6009e21885837268
Nipun3120 <nipunverma39@gmail.com>
2021-04-22 13:54:13 +0530

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,25 @@ import { signOut } from "../store/actions/authActions";
 import { makeStyles } from "@material-ui/core/styles";
 import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
 
+import '../assets/css/navbar1.css';
+
 // style here
 const useStyle = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-  },
-  rightToolbar: {
-    marginLeft: "auto",
-  },
+  // root: {
+  //   flexGrow: 1,
+  // },
+  // title: {
+  //   flexGrow: 1,
+  // },
+  // rightToolbar: {
+  //   marginLeft: "auto",
+  // },
 
-  linkUnderline: {
-    textDecoration: "none",
-    color: "whitesmoke",
-    cursor: "pointer",
-  },
+  // linkUnderline: {
+  //   textDecoration: "none",
+  //   color: "whitesmoke",
+  //   cursor: "pointer",
+  // },
 }));
 
 const Navbar = (props) => {
@@ -30,65 +32,66 @@ const Navbar = (props) => {
   const { auth } = props;
 
   const SignedInLinks = (
-    <ul style={{ listStyle: "none", display: "flex" }}>
+    <ul className='navMenu'>
       {auth.uid === "zZfVKoYwMWURII0q8tmvK6rvXvi1" ? (
-        <li>
+        <li className='li'>
           {" "}
-          <Button color="inherit">
             <NavLink to="/admin" className={classes.linkUnderline}>
-              Admin
+              <a className='hover-line'>
+                Admin
+              </a>
             </NavLink>
-          </Button>{" "}
+          {" "}
         </li>
       ) : (
         console.log("Not Admin")
       )}
 
-      <li>
+      <li className='li'>
         {" "}
-        <Button color="inherit">
           <NavLink to="/" className={classes.linkUnderline}>
-            Home
-          </NavLink>
-        </Button>{" "}
+            <a className='hover-line'>
+              Home
+            </a>
+          </NavLink>{" "}
       </li>
-      <li>
+      <li className='li'>
         {" "}
-        <Button color="inherit">
           <NavLink to="/live" className={classes.linkUnderline}>
-            Live Bidding
-          </NavLink>
-        </Button>{" "}
+            <a className='hover-line'>
+              Live Bidding
+            </a>
+          </NavLink>{" "}
       </li>
-      <li>
+      <li className='li'>
         {" "}
-        <Button color="inherit">
           <NavLink to="/silent" className={classes.linkUnderline}>
-            Silent Bidding
-          </NavLink>
-        </Button>{" "}
+            <a className='hover-line'>
+              Silent Bidding
+            </a>
+          </NavLink>{" "}
       </li>
-      <li>
+      <li className='li'>
         {" "}
-        <Button color="inherit">
           <NavLink to="/dashboard" className={classes.linkUnderline}>
-            Dashboard
-          </NavLink>
-        </Button>{" "}
+            <a className='hover-line'>
+              Dashboard
+            </a>
+          </NavLink>{" "}
       </li>
-      <li>
+      <li className='li'>
         {" "}
-        <Button color="inherit">
           <a onClick={props.signOut} className={classes.linkUnderline}>
-            Log Out
-          </a>
-        </Button>{" "}
+            <a className='hover-line'>
+              Log Out
+            </a>
+          </a>{" "}
       </li>
     </ul>
   );
 
   const SignedOutLinks = (
-    <ul style={{ listStyle: "none", display: "flex" }}>
+    <ul style={{ listStyle: "none", display: "flex" }} className='navMenu'>
       {/* <li>
         {" "}
         <Button color="inherit">
@@ -97,13 +100,13 @@ const Navbar = (props) => {
           </NavLink>
         </Button>{" "}
       </li> */}
-      <li>
+      <li className='li'>
         {" "}
-        <Button color="inherit">
           <NavLink to="/signin" className={classes.linkUnderline}>
-            Login
-          </NavLink>
-        </Button>{" "}
+            <a className='hover-line'>
+             Login
+            </a>
+          </NavLink>{" "}
       </li>
     </ul>
   );
@@ -111,19 +114,33 @@ const Navbar = (props) => {
   const links = auth.uid ? SignedInLinks : SignedOutLinks;
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color='secondary'>
-        <Toolbar>
-          <Link to="/" className="brand-logo" className={classes.linkUnderline}>
-            <Typography variant="h6" className={classes.title}>
-              IPL-AUCTION
-            </Typography>
-          </Link>
+    <div>
+      <AppBar position="static" color='secondary' className='nav'>
+          <div className='navItems'>
 
-          <div className={classes.rightToolbar}>{links}</div>
-        </Toolbar>
+            <img className='econ' src="" alt='ECON' width='6%'/>
+            <div className='alllinks'> {links} </div>
+            <img className='iplLogo' src="" alt='IPL' width='6%' />
+
+          </div>
       </AppBar>
     </div>
+
+
+
+    // <div className={classes.root}>
+    //   <AppBar position="static" color='secondary'>
+    //     <Toolbar>
+    //       <Link to="/" className="brand-logo" className={classes.linkUnderline}>
+    //         <Typography variant="h6" className={classes.title}>
+    //           IPL-AUCTION
+    //         </Typography>
+    //       </Link>
+
+    //       <div className={classes.rightToolbar}>{links}</div>
+    //     </Toolbar>
+    //   </AppBar>
+    // </div>
   );
 };
 
