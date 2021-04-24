5b9695943de61b62db8a33e710885e13afa5df0c
nipun3120 <nipunverma39@gmail.com>
2021-04-24 23:08:26 +0530

    dashboard

--- src/container/Dashboards/General.js
+++ src/container/Dashboards/General.js
@@ -1,10 +1,36 @@
 import React from "react";
 import PlayerSection from "./PlayerSection";
 
-import {Typography} from '@material-ui/core';
+import { Typography} from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import '../../assets/css/dashboard.css';
+import {makeStyles} from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    button: {
+      fontSize: '1rem',
+
+    },
+  },
+});
+
+
+const useStyles = makeStyles((theme)=>({
+  userTeamName : {
+    fontSize: '1.5rem',
+      [theme.breakpoints.down('xs')]: {
+        fontSize: '0.8rem',
+        padding: 10
+      }
+  }
+}));
+
 
 const General = ({ player }) => {
+  const classes = useStyles();
+
   console.log(player);
   console.log(player.teamBalance);
   return (
@@ -12,7 +38,7 @@ const General = ({ player }) => {
     // team details on left 
     <div className='userTeamDetails'>
       
-      <Typography color='primary' className='userTeamName' variant='h5'>{player.teamName}</Typography>
+      <Typography color='primary' variant ='h4' className={classes.userTeamName} variant='subtitle1'>{player.teamName}</Typography>
 
       <Typography color='secondary' variant='h5'>{player.initials}</Typography>
 
